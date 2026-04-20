6866b9cfc52a5f305c8f546348635a9b5f4fd53f
Leul Y. Kebede <leulykpro@gmail.com>
2026-04-20 15:29:13 -0400

    feat: implement REPL, prompt user and respond with invalid command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read from stdin...");
+
+        let command = command.trim();
+        parse_command(&command);
+    }
+}
+
+fn parse_command(command: &str) {
+    println!("{command}: command not found");
 }
